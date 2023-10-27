8a9cea34835ca573b07b36eb76e856da935e4203
fortidg <dgooch@fortinet.com>
2023-10-27 11:56:27 +0000

    added static route for trust vpc

--- resources.tf
+++ resources.tf
@@ -208,3 +208,12 @@ resource "google_compute_router_nat" "cloud_nat" {
     source_ip_ranges_to_nat = ["ALL_IP_RANGES"]
   }
 }
+
+#enable all traffic to ilb
+resource "google_compute_route" "default_route" {
+  name                   = "${var.prefix}-rt-default-via-fgt-${random_string.string.result}"
+  dest_range             = "0.0.0.0/0"
+  network                = google_compute_subnetwork.compute_subnetwork["trust-subnet-1"].name
+  next_hop_ilb           = google_compute_forwarding_rule.ifwd_rule["ilb_fwd_1"].self_link
+  priority               = 100
+}
